c9e580f46d34746d6b61849d99eaec0ba977738d
DantesRevengeReloaded <c.karakostas10@gmail.com>
2024-12-28 20:34:35 +0200

    upd

--- chat_gpt/gptpars.py
+++ chat_gpt/gptpars.py
@@ -26,25 +26,35 @@ $12.00 / 1M output** tokens
 class ChatGPTPars:
     def __init__(self):
         self.model="gpt-4o-mini"
-        self.max_tokens=4500
+        self.max_tokens=4500 # max output tokens
         self.temperature=0.7
         self.role_system = "system"
         self.role_user = "user"
+        self.tokenslimit = 27000 # limit of tokens per document
 
 
 class PdfSummerizerPars:
     def __init__(self):
         self.prompts_summarization = 'chat_gpt/prompts-roles/summarization_prompt.txt'
         self.role_of_bot_summarization = 'chat_gpt/prompts-roles/summarization_role.txt'
+        
+        # folder with total folders to be summarized, 
+        # folder with completed files and folder with files failed to be summarized
         self.input_folder = '/mnt/cf36a2d7-ecf4-46c7-a76a-5defe1ad7659/project_diplo_maria/1 RESEARCH/ACADEMIA/test'
-        self.big_text_file = 'chat_gpt/history/big_summary.txt'
         self.completed_folder = '/mnt/cf36a2d7-ecf4-46c7-a76a-5defe1ad7659/project_diplo_maria/1 RESEARCH/ACADEMIA/completed'
         self.to_be_completed_folder = '/mnt/cf36a2d7-ecf4-46c7-a76a-5defe1ad7659/project_diplo_maria/1 RESEARCH/ACADEMIA/to_be_completed'
-
+        
+        # filed where summerizes are saved, if the file becomes source for chapters
+        # be sure there are no other summeries and get wrong results
+        self.big_text_file = 'chat_gpt/history/big_summary.txt'
 
 class ChapterMakerPars:
     def __init__(self):
         self.prompts_chapter = 'chat_gpt/prompts-roles/chapter__maker_prompt.txt'
         self.role_of_bot_chapter = 'chat_gpt/prompts-roles/chapter_maker_role.txt'
-        self.paper_file = '/mnt/cf36a2d7-ecf4-46c7-a76a-5defe1ad7659/my_ai/paper.txt'
+
+        # file with the text to be used as a source, propably produced by the summarizer
+        self.paper_file = '/mnt/cf36a2d7-ecf4-46c7-a76a-5defe1ad7659/my_ai/paper.txt' 
+        
+        # file with the chapters produced by the chatbot appended ech time
         self.chapters_historicity = 'chat_gpt/history/chapters.txt'
