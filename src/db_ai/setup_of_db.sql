bd1daf1f90f225c942ca0023e4000a9495e0bb84
DantesRevengeReloaded <c.karakostas10@gmail.com>
2025-01-24 07:22:01 +0200

    UPD/

--- src/db_ai/setup_of_db.sql
+++ src/db_ai/setup_of_db.sql
@@ -4,16 +4,16 @@ CREATE SCHEMA IF NOT EXISTS ai_schema;
 -- Create table in the schema
 CREATE TABLE IF NOT EXISTS ai_schema.summaries_history (
     id SERIAL PRIMARY KEY,
-    projectname VARCHAR(50) NOT NULL,
+    projectname VARCHAR(255) NOT NULL,
     sessionid INT NOT NULL,
     prompt TEXT,
-    fileeditedname VARCHAR(255),
+    fileeditedname TEXT,
     tokencountprompt INT,
     answer TEXT,
     tokencountanswer INT,
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    model VARCHAR(50) NOT NULL,
-    modeldetails VARCHAR(255) NOT NULL,
-    type_of_prompt VARCHAR(50) NOT NULL,
-    citation VARCHAR(255)
+    model VARCHAR(255) NOT NULL,
+    modeldetails TEXT NOT NULL,
+    type_of_prompt VARCHAR(255) NOT NULL,
+    citation TEXT
 );
